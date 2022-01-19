7c1be32dd2440fbec54524fbc2af2c94967b75b3
Bailey Yu <bailey.yu@yahoo.com>
2022-01-19 13:18:16 -0800

    improved runtime to O(n)

--- longest-substring-without-repeating-characters/index.js
+++ longest-substring-without-repeating-characters/index.js
@@ -6,16 +6,26 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
-  let max = 0;
-  for (let i = 0; i < s.length; i++) {
+var lengthOfLongestSubstring = function(s) {
+    let max = 0;
+    let curI = 0;
     let setChars = new Set();
-    let curI = i;
-    while (!setChars.has(s[curI]) && curI < s.length) {
-      setChars.add(s[curI]);
-      max = Math.max(max, setChars.size);
-      curI++;
+    for(let i = 0; i < s.length; i++){
+        while(setChars.has(s[i])){
+            setChars.delete(s[curI]);
+            curI += 1;
+        }
+        setChars.add(s[i])
+        max = Math.max(max, i - curI + 1);
+
     }
-  }
-  return max;
+    return max;
 };
+
+/*
+use sliding window method
+for loop traverse through each character of s
+while the current character is in setChars, delete character of curI from setChars and increment curI
+add current char at i to set
+assign max of current max value or substring length from curI to i
+return max at end of for loop
